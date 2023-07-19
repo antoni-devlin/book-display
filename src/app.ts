87339c98d694597f24ab9b06e5e1a081fd209611
antoni-devlin <antoni.devlin@gmail.com>
2023-07-19 20:15:49 +0100

    Added Book type, and typed the parseBooks function

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-export {}; //Added to stop tsc yelling about top-level awaits...
+import { Book } from "./book";
 
 let userId = "105494066543086834878"; //My Google Books userId - this is publically available
 let bookshelves = 4; //temporary hardcoded bookshelf with id "4" - "have read"
@@ -41,5 +41,29 @@ function parseBooks(booksObject: any) {
     );
   });
 }
+constructBooks(await getBooks(4));
+
+// function parseBooks(booksObject: any) {
+//   booksObject["items"].forEach((book: any) => {
+//     let bookId = book.id;
+//     let title = book.volumeInfo.title;
+//     let authors = book.volumeInfo.authors;
+//     let author = authors[0];
+//     let datePublished = book.volumeInfo.publishedDate;
+//     let description = book.volumeInfo.description;
+//     let pageCount = book.volumeInfo.pageCount;
+//     let thumbnailPath = book.volumeInfo.imageLinks.thumbnail;
+//     let infoPageURL = book.volumeInfo.infoLink;
+//     console.log(
+//       `${bookId} - ${title} by ${author} (${datePublished}) - ${pageCount} pages long
+//       ${description}
+//       ${thumbnailPath}
+//       More info at: ${infoPageURL}
+//       `
+//     );
+//   });
+// }
+
+// parseBooks(await getBooks(4));
 
 parseBooks(await getBooks(4));
---
+++ src/book.ts
@@ -0,0 +1,11 @@
+// Type definitions
+export interface Book {
+  bookId: string;
+  authors: string[];
+  author: string;
+  datePublished: string;
+  description: string;
+  pageCount: number;
+  thumbnailPath: string;
+  infoPageUrl: string;
+}
